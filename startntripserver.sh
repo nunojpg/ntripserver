374eb8c3fe5a8fd177efa7f8fcad45bd95e820ea
Andrea Stuerze <andrea.stuerze@bkg.bund.de>
2007-08-30 15:02:19 +0000

    adapted to README
    git-svn-id: http://software.rtcm-ntrip.org/svn/trunk/ntripserver@490 a76a543e-5a0c-11df-b9b2-fd73e79bcebd

--- startntripserver.sh
+++ startntripserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# $Id$
+# $Id: startntripserver.sh,v 1.1 2007/08/30 07:45:59 stoecker Exp $
 # Purpose: Start ntripserver
 
-while true; do ./ntripserver -a www.euref-ip.net -p 80 -m Mountpoint -c password -M 1 -b 19200 -i /dev/gps; sleep 20; done
+while true; do ./ntripserver -M 1 -i /dev/ttys0 -b 9600 -O 2 -a www.euref-ip.net -p 2101 -m Mount2 -n serverID -c serverPass; sleep 60; done
